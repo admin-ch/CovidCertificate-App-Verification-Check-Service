13d1220ca5ec3888a17e417fe97c9ccc1c77325f
lmeinen <lmeinen@student.ethz.ch>
2021-07-02 11:57:04 +0200

    Updated tests

--- ch-covidcertificate-backend-verification-check/ch-covidcertificate-backend-verification-check-ws/src/test/java/ch/admin/bag/covidcertificate/backend/verification/check/ws/controller/VerificationControllerTest.java
+++ ch-covidcertificate-backend-verification-check/ch-covidcertificate-backend-verification-check-ws/src/test/java/ch/admin/bag/covidcertificate/backend/verification/check/ws/controller/VerificationControllerTest.java
@@ -7,7 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import ch.admin.bag.covidcertificate.backend.verification.check.model.HCertPayload;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +16,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 
 class VerificationControllerTest extends BaseControllerTest {
 
-    private static final Logger logger =
-        LoggerFactory.getLogger(VerificationControllerTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(VerificationControllerTest.class);
 
     private static final String BASE_URL = "/v1";
     private static final String VERIFY_ENDPOINT = "/verify";
@@ -25,28 +24,45 @@ class VerificationControllerTest extends BaseControllerTest {
     @Test
     void helloTest() throws Exception {
         final MockHttpServletResponse response =
-            mockMvc.perform(get(BASE_URL + "/").accept(MediaType.TEXT_PLAIN))
-                .andExpect(status().is2xxSuccessful())
-                .andReturn()
-                .getResponse();
+                mockMvc.perform(get(BASE_URL + "/").accept(MediaType.TEXT_PLAIN))
+                        .andExpect(status().is2xxSuccessful())
+                        .andReturn()
+                        .getResponse();
 
         assertNotNull(response);
         assertEquals(
-            "Hello from CH CovidCertificate Verification Check WS", response.getContentAsString());
+                "Hello from CH CovidCertificate Verification Check WS",
+                response.getContentAsString());
     }
 
     @Test
-    void verificationTest() throws Exception {
+    void invalidCertTest() throws Exception {
         var hCertPayload = new HCertPayload();
         hCertPayload.setHcert("HC1:example");
         final MockHttpServletResponse response =
-            mockMvc.perform(
-                post(BASE_URL + VERIFY_ENDPOINT)
-                    .content(objectMapper.writeValueAsString(hCertPayload))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful())
-                .andReturn()
-                .getResponse();
+                mockMvc.perform(
+                                post(BASE_URL + VERIFY_ENDPOINT)
+                                        .content(objectMapper.writeValueAsString(hCertPayload))
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isBadRequest())
+                        .andReturn()
+                        .getResponse();
+    }
+
+    @Test
+    @Disabled("Not implemented")
+    void validCertTest() throws Exception {
+        var hCertPayload = new HCertPayload();
+        hCertPayload.setHcert("HC1:example");
+        final MockHttpServletResponse response =
+                mockMvc.perform(
+                                post(BASE_URL + VERIFY_ENDPOINT)
+                                        .content(objectMapper.writeValueAsString(hCertPayload))
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().is2xxSuccessful())
+                        .andReturn()
+                        .getResponse();
     }
 }
